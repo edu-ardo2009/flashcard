532859a613a50117a1b9529f3c22290b608d629e
edu-ardo2009 <eduardo.padovan@escola.pr.gov.br>
2025-06-13 17:40:17 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,54 @@
+criaCartao(
+    'História',
+    'Em que ano o Corinthians foi fundado?',
+    '1910.'
+)
+
+criaCartao(
+    'Libertadores',
+    'Em que ano o Corinthians conquistou sua primeira Libertadores?',
+    '2012.'
+)
+
+criaCartao(
+    'Mundial',
+    'Quantos Mundiais o Corinthians tem?',
+    'Dois (2000 e 2012).'
+)
+
+criaCartao(
+    'Ídolos',
+    'Quem é o maior artilheiro da história do Corinthians?',
+    'Cláudio, com 306 gols.'
+)
+
+criaCartao(
+    'Rivais',
+    'Qual é o principal rival do Corinthians?',
+    'Palmeiras.'
+)
+
+criaCartao(
+    'Estádio',
+    'Qual é o nome da arena do Corinthians?',
+    'Neo Química Arena.'
+)
+
+criaCartao(
+    'Campeonato Brasileiro',
+    'Quantos títulos do Brasileirão o Corinthians possui?',
+    'Sete títulos.'
+)
+
+criaCartao(
+    'Mascote',
+    'Qual é o mascote do Corinthians?',
+    'Mosqueteiro.'
+)
+
+criaCartao(
+    'Torcida',
+    'Qual o apelido da torcida do Corinthians?',
+    'Fiel Torcida.'
+)
 
